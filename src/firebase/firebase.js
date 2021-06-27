dbf79792fc85f289c41c1e91a29adb503510eb27
jstloyal <jstloyalty@gmail.com>
2021-06-28 00:03:24 +0100

    write firebase functions

---
+++ src/firebase/firebase.js
@@ -0,0 +1,142 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-FxqXfxYjPcPGdz1wspNtSeIzPwK7yTo",
+  authDomain: "wt-gadget.firebaseapp.com",
+  databaseURL: "https://wt-gadget-default-rtdb.firebaseio.com",
+  projectId: "wt-gadget",
+  storageBucket: "wt-gadget.appspot.com",
+  messagingSenderId: "406384254572",
+  appId: "1:406384254572:web:13574b99c22157364d8249"
+};
+
+class Firebase {
+  constructor(){
+    app.initializeApp(firebaseConfig);
+    this.storage = app.storage();
+    this.db = app.firestore();
+    this.auth = app.auth();
+  }
+
+  createAccount = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
+  signIn = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
+  signInWithGoogle = () => this.auth.signInWithPopup(new app.auth.GoogleAuthProvider());
+  signOut = () => this.auth.signOut();
+
+  addUser = (id, user) => { this.db.collection('users').doc(id).set(user) };
+  getUser = id => this.db.collection('users').doc(id).get();
+
+  passwordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  changePassword = (currentPassword, newPassword) => {
+    return new Promise((resolve, reject) => {
+      this.reauthenticate(currentPassword).then(() => {
+        const user = this.auth.currentUser;
+        user.updatePassword(newPassword).then(() => {
+          resolve("Password Updated Successfully");
+        }).catch(error => reject(error));
+      }).catch(error => reject(error));
+    });
+  };
+
+  updateEmail = (currentPassword, newEmail) => {
+    return new Promise((resolve, reject) => {
+      this.reauthenticate(currentPassword).then(() => {
+        const user = this.auth.currentUser;
+        user.updateEmail(newEmail).then(() => {
+          resolve("Email Updated Successfully");
+        }).catch(error => reject(error));
+      }).catch(error => reject(error));
+    });
+  };
+
+  updateProfile = (id, updates) => this.db.collection('users').doc(id).update(updates);
+
+  reauthenticate = currentPassword => {
+    const user = this.auth.currentUser;
+    const cred = app.auth.EmailAuthProvider.credential(user.email, currentPassword);
+
+    return user.reauthenticateWithCredential(cred);
+  };
+
+  onAuthStateChanged = () => {
+    return new Promise ((resolve, reject) => {
+      this.auth.onAuthStateChanged((user) => {
+        if (user) {
+          return resolve(user);
+        } else {
+          return reject(new Error('Auth State Changed Failed'));
+        }
+      });
+    });
+  };
+
+  setAuthPersistence = () => this.auth.setPersistence(app.auth.Auth.Persistence.LOCAL);
+
+  getproducts = (lastRefkey) => {
+    let didTimeout = false;
+
+    return new Promise(async (resolve, reject) => {
+      if (lastRefkey) {
+        try{
+          const query = this.db.collection('products').orderBy(app.firestore.FieldPath.documentId()).startAfter(lastRefkey).limit(12);
+          
+          const snapshot = await query.get();
+          const products = [];
+          snapshot.forEach(doc => products.push({ id: doc.id, ...doc.data() }));
+          const lastKey = snapshot.docs[snapshot.docs.length - 1];
+
+          resolve({ products, lastKey });
+        }catch(e){
+          reject(':( Failed to fetch products.')
+        }
+      } else {
+        const timeout = setTimeout(() => {
+          didTimeout = true;
+          reject('Request timeout, please try again!');
+        }, 15000)
+        try{
+          const totalQuery = await this.db.collection('products').get();
+          const total = totalQuery.docs.length;
+          const query = this.db.collection('products').orderBy(app.firestore.FieldPath.documentId()).limit(12);
+          const snapshot = await query.get();
+
+          clearTimeout(timeout);
+          if (!didTimeout){
+            const products = [];
+            snapshot.forEach(doc => products.push({id: doc.id, ...doc.data()}));
+            const lastKey = snapshot.docs[snapshot.docs.length - 1];
+            resolve({products, lastKey, total})
+          }
+        }catch(e) {
+          if (didTimeout) return;
+          console.log('Failed to fetch products: an error occure while trying to fetch products or there is no products.', e);
+          reject('failed to fetch products.')
+        }
+      }
+    });
+  }
+
+  addProduct = (id, product) => this.db.collection('products').doc(id).set(product);
+
+  generateKey = () => this.db.collection('products').doc().id;
+
+  storeImage = async (id, folder, imageFile) => {
+    const snapshot = await this.storage.ref(folder).child(id).put(imageFile);
+    const downloadURL = await snapshot.ref.getDownloadURL();
+    return downloadURL;
+  }
+
+  deleteImage = id => this.storage.ref('products').child(id).delete();
+
+  editProduct = (id, updates) => this.db.collection('products').doc(id).update(updates);
+
+  removeProduct = id => this.db.collection('products').doc(id).delete();
+}
+
+const firebase = new Firebase();
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,4 +13,14 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
+      console.log('SW registered: ', registration);
+
+    }).catch((registrationError) => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
