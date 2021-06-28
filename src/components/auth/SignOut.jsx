9413a1282dba721ee73df5fed5ffb36f75f228eb
jstloyal <jstloyalty@gmail.com>
2021-06-28 01:36:43 +0100

    add auth component

---
+++ src/components/auth/SignOut.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { signOut } from 'actions/authActions';
+
+const SignOut = (props) => {
+  const dispatch = useDispatch();
+
+  const onSignOut = () => {
+    dispatch(signOut());
+  };
+
+  return props.children({ onSignOut });
+};
+
+export default SignOut;
