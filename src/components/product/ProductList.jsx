94fa349cc49ae77a8227d1b4f3f2cdaa1c3f57fe
jstloyal <jstloyalty@gmail.com>
2021-06-28 01:30:11 +0100

    add product components

---
+++ src/components/product/ProductList.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import CircularProgress from '../ui/CircularProgress';
+import MessageDisplay from '../ui/MessageDisplay';
+
+
+import { getProducts } from 'actions/productActions';
+import { isLoading as dispatchIsLoading } from 'actions/appActions';
+
+const ProductList = ({ 
+  isLoading, 
+  requestStatus, 
+  productsLength,
+  filteredProductsLength, 
+  lastRefKey,
+  totalItems,
+  dispatch,
+  location,
+  children 
+}) => {
+  const [lastScrollPos, setLastScrollPos] = useState(0);
+  const [isFetching, setFetching] = useState(false);
+  const [scrolledAtBottom, setScrolledAtBottom] = useState(false);
+  const [columnCount, setColumnCount] = useState(6);
+
+  useEffect(() => {
+    if (productsLength === 0)  {
+      fetchProducts();
+    }
+  
+    return () => dispatch(dispatchIsLoading(false));
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, lastScrollPos);
+    setFetching(false);
+  }, [lastRefKey]); // watch for changes on lastRefKey, if it changes that means new products have been fetched.
+
+  useEffect(() => {
+    window.addEventListener('scroll', watchForScroll);
+
+    return () => window.removeEventListener('scroll', watchForScroll);
+  }, [lastRefKey, isLoading]); // re-add event listener since the height of the window has increased for fetching new items.
+
+  const watchForScroll = () => {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = winScroll / height;  // value of 1 means it's at the bottom
+    
+    if (scrolled === 1 && !!lastRefKey && !isLoading && productsLength < totalItems ) {
+      setLastScrollPos(window.pageYOffset);
+      setScrolledAtBottom(true);
+      window.removeEventListener('scroll', watchForScroll);
+    } 
+  };
+
+  const fetchProducts = () => {
+    setFetching(true);
+    dispatch(getProducts(lastRefKey));
+  };
+  
+  return filteredProductsLength === 0 && !isLoading && !requestStatus ? (
+    <MessageDisplay 
+        message="The are no items found."
+        desc="Try using correct filters or keyword."
+    />
+  ) : requestStatus ? (
+    <MessageDisplay 
+        message={requestStatus}
+        action={fetchProducts}
+        buttonLabel="Try Again"
+    />
+  ) : (
+    <>
+    {children}
+    {((scrolledAtBottom || location.pathname === '/admin/products') && productsLength < totalItems) && (
+      <div className="d-flex-center padding-l">
+        <button 
+            className="button button-small"
+            disabled={isFetching}
+            onClick={fetchProducts}
+        >
+          {isFetching ? 'Fetching Items...' : 'Fetch More Items'}
+        </button>
+      </div>
+    )}
+    </>
+  )
+};
+
+ProductList.propType = {
+  isLoading: PropTypes.bool.isRequired,
+  requestStatus: PropTypes.string.isRequired,
+  productsLength: PropTypes.number.isRequired,
+  filteredProductsLength: PropTypes.number.isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+export default ProductList;
+
---
+++ src/components/product/ProductModalDetails.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ImageLoader from '../ui/ImageLoader';
+import { addToBasket, removeFromBasket } from 'actions/basketActions';
+import { displayMoney, displayActionMessage } from 'helpers/utils'; 
+
+const ProductModalDetails = ({ product, dispatch, foundOnBasket }) => {
+  const onAddToBasket = () => {
+    if (foundOnBasket(product.id)) {
+      dispatch(removeFromBasket(product.id));
+      displayActionMessage('Item removed from basket', 'info');
+    } else {
+      dispatch(addToBasket(product));
+      displayActionMessage('Item added to basket', 'success');
+    }
+  };
+
+  return !product ? null : (
+    <div className="product-modal">
+      <div className="product-modal-image-wrapper">
+        <ImageLoader 
+            alt={product.name}
+            className="product-modal-image"
+            src={product.image} 
+        />
+      </div>
+      <div className="product-modal-details">
+        <h3>{product.name}</h3>
+        <span className="text-subtle">Brand: &nbsp;</span> 
+        <span><strong>{product.brand}</strong></span>
+        <br/>
+        <br/>
+        <span>{product.description}</span>
+        <br/>
+        <h1>{displayMoney(product.price)}</h1>
+        <div className="product-modal-action">
+          <button 
+              className={`button button-small ${foundOnBasket(product.id) ? 'button-border button-border-gray' : ''}`} 
+              onClick={onAddToBasket}
+          >
+            {foundOnBasket(product.id) ? 'Remove From Basket' : 'Add To Basket'}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProductModalDetails.propType = {
+  product: PropTypes.object.isRequired,
+  addToBasket: PropTypes.func.isRequired,
+  foundOnBasket: PropTypes.func.isRequired
+};
+
+export default ProductModalDetails;
